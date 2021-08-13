a97ee398ca0a11484d802ff9848cfcec01cd0b24
Linus Ritzmann <ritzmlin@students.zhaw.ch>
2021-08-13 15:19:36 +0200

    added getter for category by name to vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,43 @@
 import { createStore } from "vuex";
 import { Content } from "@/types/domain/Content";
 import axios from "axios";
+import { Category } from "@/types/domain/Category";
+
+function flattenCategoryTree(category: Category): Array<Category> {
+  if (category.subcategories) {
+    return [category, ...category.subcategories.flatMap(flattenCategoryTree)];
+  } else {
+    return [category];
+  }
+}
 
 export default createStore({
   state: {
-    content: null as Content | null
+    content: null as Content | null,
+    categories: new Map<string, Category>()
   },
   mutations: {
     addContent(state, content: Content) {
       state.content = content;
+    },
+    addCategory(state, category: Category) {
+      state.categories.set(category.title, category);
     }
   },
   actions: {
     async loadContent({ commit }) {
-      const { statusText, data } = await axios.get("/content");
+      const { statusText, data } = await axios.get<Content>("/content");
       if (statusText.toLowerCase() === "ok") {
         commit("addContent", data);
+        data.categories
+          .flatMap(flattenCategoryTree)
+          .forEach(cat => commit("addCategory", cat));
       }
+    }
+  },
+  getters: {
+    getCategory: state => (title: string): Category | undefined => {
+      return state.categories.get(title);
     }
   }
 });
